f48a672006214dfbb314d397b7df52516a1c0835
tmoiseyenko06 <tmoiseyenko06@gmail.com>
2025-02-19 18:37:50 -0500

    added response

--- backend/services/user_services.py
+++ backend/services/user_services.py
@@ -2,24 +2,9 @@ from pocketbase import PocketBase as pb
 import os
 from dotenv import load_dotenv
 from .authentication_services import hash_password
+from .database_services import verify_db, client
+from flask import jsonify
 
-load_dotenv()
-# get superuser data from enviorment
-admin_email = os.getenv('ADMIN_EMAIL')
-admin_password = os.getenv('ADMIN_PASSWORD')
-
-client = pb('http://127.0.0.1:8090') # Enter URL for pocketbase here
-
-
-def verify_db(func):
-    def wrapper(*args,**kwargs):
-        superuser_data = client.admins.auth_with_password(admin_email,admin_password)
-        superuser_data.is_valid
-        result = func(*args,**kwargs)
-        client.auth_store.clear()
-        return result
-    return wrapper
-        
 
 @verify_db
 def create_user(email,password,emailVisibility=False,verified=False,cityOfficial=False):
@@ -34,4 +19,4 @@ def create_user(email,password,emailVisibility=False,verified=False,cityOfficial
         "cityOfficial": cityOfficial
     }
     )
-    return result
+    return jsonify({"id":result.id,"email":result.email}), 201
