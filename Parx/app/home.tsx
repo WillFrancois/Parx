f9ac119fd509a75065febd29f4b3fee1d829cb02
b-smith94 <byrondsmith9@gmail.com>
2025-02-25 11:01:03 -0500

    swapped routes so Login would appear first

---
+++ Parx/app/home.tsx
@@ -0,0 +1,17 @@
+import { Text, View, Button } from "react-native";
+import { useRouter } from 'expo-router'
+
+export default function Index() {
+  const router = useRouter();
+
+  return (
+    <View style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <Text style={{ fontSize: 24, marginBottom: 20  }}>Home</Text>
+      <Button title="Logout" onPress={() => router.push("/")} />
+    </View>
+  )
+}
--- Parx/app/index.tsx
+++ Parx/app/index.tsx
@@ -1,17 +1,30 @@
-import { Text, View, Button } from "react-native";
-import { useRouter } from 'expo-router'
+import { View, Text, Button, TextInput } from "react-native";
+import { useRouter } from 'expo-router';
+import React, {useState} from 'react';
 
-export default function Index() {
-  const router = useRouter();
+export default function LoginScreen() {
+    const router = useRouter();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  return (
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}>
-      <Text style={{ fontSize: 24, marginBottom: 20  }}>Home</Text>
-      <Button title="Go to Login" onPress={() => router.push("/login")} />
-    </View>
-  )
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text style={{ fontSize: 24, marginBottom: 20 }}>Login Page</Text>
+            <Text style={{ fontSize: 17, marginBottom: 10 }}>Email</Text>
+            <TextInput 
+                style={{ height: 40, padding: 5 }}
+                placeholder="Enter your Email"
+                onChangeText={newEmail => setEmail(newEmail)}
+                defaultValue={email}
+            />
+            <Text style={{ fontSize: 17, marginBottom: 10 }}>Password</Text>
+            <TextInput 
+                style={{ height: 40, padding: 5 }}
+                placeholder="Enter your Password"
+                onChangeText={newPassword => setPassword(newPassword)}
+                defaultValue={password}
+            />
+            <Button title="Submit" onPress={() => router.push("/home")} />
+        </View>
+    )
 }
