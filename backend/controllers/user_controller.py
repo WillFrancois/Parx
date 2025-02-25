38277081e50d0c5a786f6b9dd4624792276340da
WillFrancois <williamjcfrancois@gmail.com>
2025-02-24 19:36:04 -0500

    Created login route and created pepper environment variable

--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -1,6 +1,11 @@
-from services.user_services import create_user
+from services.user_services import create_user, verify_user
 from flask import request
 
 def user_create():
     data = request.json
     return create_user(data['email'],data['password'])
+
+def user_verify():
+    data = request.json
+    print(data['email'], data['password'])
+    return verify_user(data['email'], data['password'])
--- backend/services/authentication_services.py
+++ backend/services/authentication_services.py
@@ -1,9 +1,9 @@
 import bcrypt
+from .database_services import db_pepper
 
 def hash_password(password):
     password = password.encode('utf-8')
-    salt = bcrypt.gensalt()
-    password_hashed = bcrypt.hashpw(password,salt)
+    password_hashed = bcrypt.hashpw(password, db_pepper)
     return password_hashed
 
 def check_password_hash(password,hash):
