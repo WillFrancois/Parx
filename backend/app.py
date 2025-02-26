12d23ca33d7c6c0d71903068af94112841e01c7f
b-smith94 <byrondsmith9@gmail.com>
2025-02-26 12:33:49 -0500

    added CORS functionality

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.user_routes import user_blueprint
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r"/*": {"origins": "*"}})
     blueprint_config(app)
     return app
 
 def blueprint_config(app):
     app.register_blueprint(user_blueprint,url_prefix='/user')
 
+@user_blueprint.route('', methods=['OPTIONS'])
+def options():
+    return '', 200
+
 app = create_app()
-app.run(debug=True)
+app.run(host="0.0.0.0", port=5000, debug=True)
