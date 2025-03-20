2f0cc5c94ea81cb271ffe32dba4a8a859444130c
B-smith94 <byrondsmith9@gmail.com>
2025-03-20 15:49:28 -0400

    .

--- Parx/config.ts
+++ Parx/config.ts
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const IPV4 = "192.168.x.x"
+const IPV4 = "192.168.0.40"
 
 export const API_BASE_URL = `http://${IPV4}:5000` // replace .x.x with IPV4 digits
 export const pb = new PocketBase(`http://${IPV4}:8090`)
