7f8ee70d7c7f985a6ff97eb4baeeb1a51263a465
WillFrancois <williamjcfrancois@gmail.com>
2025-03-24 19:51:26 -0400

    Added route to receive information about a reservation based on ID

--- backend/controllers/reservation_controller.py
+++ backend/controllers/reservation_controller.py
@@ -1,7 +1,10 @@
-from services.reservation_services import add_reservation
+from services.reservation_services import add_reservation, view_reservation
 from flask import request
 
 def reservation_add():
     data = request.json
     return add_reservation(data['plate_number'], data['time_requested'], data['parking_lot_id'])
 
+def reservation_view():
+    data = request.json
+    return view_reservation(data['plate_number'])
