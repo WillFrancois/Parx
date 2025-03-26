03b83ad089bcd41de60d8a15392ebea3771abd9d
seandesiree <sean.desiree@gmail.comgit config --global user.email sean.desiree@gmail.comgit versioncdgit config --global user.email sean.desiree@gmail.com>
2025-03-26 18:08:31 -0400

    updated the zip code search function and double tap feature

--- Parx/config.ts
+++ Parx/config.ts
@@ -1,8 +1,8 @@
 import PocketBase from 'pocketbase';
 
-const IPV4 = "192.168.x.x" // replace .x.x with IPV4 digits
+const IPV4 = "192.168.1.46" // replace .x.x with IPV4 digits
 
 export const API_BASE_URL = `http://${IPV4}:5000` 
 export const pb = new PocketBase(`http://${IPV4}:8090`)
-export const MAPBOX_API_KEY = "add API key"
+export const MAPBOX_API_KEY = "pk.eyJ1Ijoic2VhbmRlZXJheSIsImEiOiJjbThxNjc1ajgwamt5MmlxNGFzdzI1Z2VtIn0.ljLsXjL7EdvxStUvcOPfIg"
 
