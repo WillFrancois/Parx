ac8fc629f4b0a8e29e45b527bad5e21c49bb3b99
seandesiree <sean.desiree@gmail.comgit config --global user.email sean.desiree@gmail.comgit versioncdgit config --global user.email sean.desiree@gmail.com>
2025-04-01 19:33:43 -0400

    allows all emails with cityofficial status

--- Parx/config.ts
+++ Parx/config.ts
@@ -1,8 +1,8 @@
 import PocketBase from 'pocketbase';
 
-const IPV4 = "192.168.1.46" // replace .x.x with IPV4 digits
+const IPV4 = "192.168.x.x" // replace .x.x with IPV4 digits
 
 export const API_BASE_URL = `http://${IPV4}:5000` 
 export const pb = new PocketBase(`http://${IPV4}:8090`)
-export const MAPBOX_API_KEY = "pk.eyJ1Ijoic2VhbmRlZXJheSIsImEiOiJjbTh4bmNsdG8wNWdkMnRxM245N3J0NmN6In0.orMbsDxjX-iG7SRA_4C-Ug"
+export const MAPBOX_API_KEY = "enter API"
 
